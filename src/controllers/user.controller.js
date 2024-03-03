61598f115d76febb2ef68047bd40905398e06481
Nareshkumar124 <nkk124421@gmail.com>
2024-03-03 16:25:38 +0530

    chnage in cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -154,6 +154,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite:"none"
     };
     
 
@@ -223,6 +224,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite:"none"
     };
 
     res.status(200)
