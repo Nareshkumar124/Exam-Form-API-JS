e7f5dac35997fd5f984bcf930c3ed5f7373873c8
Nareshkumar124 <nkk124421@gmail.com>
2024-03-03 15:44:31 +0530

    chnage in cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const app = express();
 //CORS
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        // origin: ,   // process.env.CORS_ORIGIN
+        credentials:true
     })
 );
 
